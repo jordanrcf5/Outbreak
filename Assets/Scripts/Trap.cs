b1b8ad0009ac5851636d83cf9a0fa2e90edbebce
Jordan Ferrer <jordanrcf25@gmail.com>
2025-12-09 02:09:56 -0800

    Spike and bounce traps

---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    public float bounceForce = 10f;
+    public int damage = 1;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            HandlePlayerBounce(collision.gameObject);
+
+        }
+    }
+
+    private void HandlePlayerBounce(GameObject player)
+    {
+        Rigidbody2D rb = player.GetComponent<Rigidbody2D>();
+
+        if (rb)
+        {
+            // reset player velocity
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
+
+            // apply bounce force
+            rb.AddForce(Vector2.up * bounceForce, ForceMode2D.Impulse);
+        }
+    }
+}
